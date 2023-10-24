44d76c1bbff8a1d975ba63c945248c221ed15095
nanvon <nanvon@foxmail.com>
2023-10-24 11:08:25 +0800

    feat: add useChart echarts hook

---
+++ packages/useChart/index.ts
@@ -0,0 +1,36 @@
+import * as echarts from 'echarts';
+import { onBeforeUnmount, ref, markRaw } from 'vue';
+
+/**
+ * @description echarts hook
+ * @param {any} element:HTMLElement
+ * @returns {any}
+ */
+export const useChart = (domId: string): any => {
+  const myChart = ref<echarts.ECharts | null>(null);
+  const showChart = ref<boolean>(true);
+
+  const chartListener = () => {
+    if (showChart.value) {
+      console.log('echarts hook: chart resize');
+      myChart.value?.resize();
+    }
+  };
+
+  const initChart = () => {
+    const chartDom = document.getElementById(domId) as HTMLElement;
+    myChart.value = markRaw(echarts.init(chartDom));
+    window.addEventListener('resize', chartListener);
+  };
+
+  onBeforeUnmount(() => {
+    window.removeEventListener('resize', chartListener);
+    if (myChart.value != null) {
+      myChart.value.clear();
+      myChart.value.dispose();
+      myChart.value = null;
+    }
+  });
+
+  return { myChart, showChart, initChart, chartListener };
+};
